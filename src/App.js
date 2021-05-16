10f5fd6ef72aa6b34aafdf40368d13b8098829c6
Sardor <sardorbakhrommov3101@gmail.com>
2021-05-16 23:15:59 +0500

    'ui'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import "./App.css";
+import Input from "./components/UI/Input/Input";
+import Button from "./components/UI/Button/Button";
 
 function App() {
-  return <div className="App"></div>;
+    const elementConfig = {type: 'text', placeholder: 'Search'}
+    const options = [
+        {value: 'cars', displayValue: 'Cars'},
+        {value: 'jobs', displayValue: 'Jobs'},
+        {value: 'services', displayValue: 'Services'}
+    ]
+
+    return <div className="App">
+        <Input
+            elementConfig={elementConfig}
+            elementType='input'
+            label='Search products'
+        />
+        <Input
+            elementType='select'
+            options={options}
+            label='Choose'
+        />
+        <Button
+            btnType='Main'
+        >
+            Click
+        </Button>
+
+    </div>;
 }
 
 export default App;
