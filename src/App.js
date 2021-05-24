c0ba41004953a32ad1e1d7abc6a4df93d6f9b06f
diyorbek0309 <diyorbekolimov2000@gmail.com>
2021-05-24 19:28:38 +0500

    added redux store

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
+import { useEffect } from "react";
 import "./App.css";
+import { Provider } from "react-redux";
+import store from "./store";
+import { changeLanguage } from "./redux/actions/lang";
 import Input from "./components/UI/Input/Input";
 import Button from "./components/UI/Button/Button";
 
-function App() {
-    const elementConfig = {type: 'text', placeholder: 'Search'}
-    const options = [
-        {value: 'cars', displayValue: 'Cars'},
-        {value: 'jobs', displayValue: 'Jobs'},
-        {value: 'services', displayValue: 'Services'}
-    ]
+const App = () => {
+  useEffect(() => {
+    changeLanguage("uz");
+  }, []);
 
-    return <div className="App">
-        <Input
-            elementConfig={elementConfig}
-            elementType='input'
-            label='Search products'
-        />
-        <Input
-            elementType='select'
-            options={options}
-            label='Choose'
-        />
-        <Button
-            btnType='Main'
-        >
-            Click
-        </Button>
+  const elementConfig = { type: "text", placeholder: "Search" };
+  const options = [
+    { value: "cars", displayValue: "Cars" },
+    { value: "jobs", displayValue: "Jobs" },
+    { value: "services", displayValue: "Services" },
+  ];
 
-    </div>;
-}
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <Input
+          elementConfig={elementConfig}
+          elementType="input"
+          label="Search products"
+        />
+        <Input elementType="select" options={options} label="Choose" />
+        <Button btnType="Main">Click</Button>
+      </div>
+    </Provider>
+  );
+};
 
 export default App;
--- src/store.js
+++ src/store.js
@@ -1,16 +1,17 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from "./reducers";
-
-const initialState = {};
+import rootReducer from "./redux/reducers/index";
 
 const middleware = [thunk];
 
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
+
 const store = createStore(
   rootReducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
